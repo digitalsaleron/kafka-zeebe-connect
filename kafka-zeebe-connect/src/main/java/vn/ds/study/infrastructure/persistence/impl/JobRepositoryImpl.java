87496dac38677d2f86bac3b72e15c6accb13996b
Tran Quang Minh <minh.tran.1@swisspost.com>
2021-09-09 01:41:29 +0700

    implement high availability job repository

--- kafka-zeebe-connect/src/main/java/vn/ds/study/infrastructure/persistence/impl/JobRepositoryImpl.java
+++ kafka-zeebe-connect/src/main/java/vn/ds/study/infrastructure/persistence/impl/JobRepositoryImpl.java
@@ -23,6 +23,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -33,7 +34,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.binder.kafka.properties.KafkaBinderConfigurationProperties;
+import org.springframework.cloud.stream.binder.kafka.properties.KafkaBindingProperties;
 import org.springframework.cloud.stream.binder.kafka.properties.KafkaExtendedBindingProperties;
+import org.springframework.cloud.stream.binding.BindingService;
+import org.springframework.cloud.stream.config.BinderProperties;
+import org.springframework.cloud.stream.config.BindingServiceProperties;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -55,6 +60,10 @@ public class JobRepositoryImpl implements JobRepository{
     
     private static final Logger LOGGER = LoggerFactory.getLogger(JobRepositoryImpl.class);
 
+    private static final String BINDING_NAME_DEFAULT = "__job-instances-in-0";
+    
+    private static final String TOPIC_NAME_DEFAULT = "__job-instances";
+    
     private Map<String, JobInfo> jobIntances = new ConcurrentHashMap<>();
     
     @Autowired
@@ -72,11 +81,14 @@ public class JobRepositoryImpl implements JobRepository{
     @Autowired
     private KafkaExtendedBindingProperties kafkaExtendedBindingProperties;
     
+    @Autowired
+    private BindingServiceProperties bindingServiceProperties;
+    
     @PostConstruct
     @SuppressWarnings("unchecked")
     private void initialize() throws IOException {
-        final Properties properties = new Properties();
-        properties.putAll(kafkaBinderConfigurationProperties.mergedConsumerConfiguration());
+        final Properties properties = this.createKafkaConsumerProperties();
+        
         final KafkaConsumer<byte[], byte[]> kafkaConsumer = KafkaConsumerBuilder.prepare(properties).build();
         
         final long maxPollInterval = Long.parseLong((String) properties.get(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG)); 
@@ -107,11 +119,54 @@ public class JobRepositoryImpl implements JobRepository{
         kafkaConsumer.close();
     }
 
+    private Properties createKafkaConsumerProperties() {
+        final Properties properties = new Properties();
+
+        final Map<String, BinderProperties> binderProperties = this.bindingServiceProperties.getBinders();
+        final Map<String, Object> binderAddresses = new HashMap<>();
+        binderProperties.forEach((key, value) -> {
+            this.flatten(null, value.getEnvironment(), binderAddresses);
+        });
+        
+        
+        properties.putAll(kafkaBinderConfigurationProperties.mergedConsumerConfiguration());
+        
+        
+        KafkaBindingProperties kafkaBindingProperties = kafkaExtendedBindingProperties.getBindings().get(
+            BINDING_NAME_DEFAULT);
+    
+        if (kafkaBindingProperties != null) {
+            properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                kafkaBindingProperties.getConsumer().getStartOffset().name());
+        }
+        return properties;
+    }
+    
+    private NewTopic createNewTopicIfNecessary() {
+        final int minPartition = this.kafkaBinderConfigurationProperties.getMinPartitionCount();
+        final short replicationFactor = this.kafkaBinderConfigurationProperties.getReplicationFactor();
+        String topicName = this.bindingServiceProperties.getBindingDestination(BINDING_NAME_DEFAULT);
+        topicName = topicName != null ? topicName : TOPIC_NAME_DEFAULT;
+        final NewTopic topic = new NewTopic(topicName, minPartition, replicationFactor);
+        topic.configs(this.kafkaExtendedBindingProperties.getBindings().get(
+            BINDING_NAME_DEFAULT).getConsumer().getTopic().getProperties());
+        return topic;
+    }
+
     private boolean hasReadToEndOffsets(
         final Map<TopicPartition, Long> endOffsets,
         final KafkaConsumer<byte[], byte[]> kafkaConsumer) {
         endOffsets.entrySet().removeIf(entry -> kafkaConsumer.position(entry.getKey()) >= entry.getValue());
         return endOffsets.isEmpty();
+    }
+    
+    private void flatten(String propertyName, Object value, Map<String, Object> binderProperties) {
+        if (value instanceof Map) {
+            ((Map<Object, Object>) value).forEach(
+                (k, v) -> flatten((propertyName != null ? propertyName + "." : "") + k, v, binderProperties));
+        } else {
+            binderProperties.put(propertyName, value.toString());
+        }
     }
 
     @Override
