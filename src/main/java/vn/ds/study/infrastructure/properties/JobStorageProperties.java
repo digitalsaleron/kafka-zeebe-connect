b57d97ffcc1f57de544ae85fceb514cf2313d2af
Minh Tran <wisekingtran@gmail.com>
2021-09-12 23:06:36 +0700

    Restructure the POM project

---
+++ src/main/java/vn/ds/study/infrastructure/properties/JobStorageProperties.java
@@ -0,0 +1,30 @@
+/*
+ * Class: JobCacheProperties
+ *
+ * Created on Sep 10, 2021
+ *
+ * (c) Copyright Swiss Post Solutions Ltd, unpublished work
+ * All use, disclosure, and/or reproduction of this material is prohibited
+ * unless authorized in writing.  All Rights Reserved.
+ * Rights in this program belong to:
+ * Swiss Post Solution.
+ * Floor 4-5-8, ICT Tower, Quang Trung Software City
+ */
+package vn.ds.study.infrastructure.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "job-storage")
+public class JobStorageProperties {
+    
+    private boolean jobRemovalEnabled;
+
+    public boolean isJobRemovalEnabled() {
+        return jobRemovalEnabled;
+    }
+
+    public void setJobRemovalEnabled(boolean jobRemovalEnable) {
+        this.jobRemovalEnabled = jobRemovalEnable;
+    }
+    
+}
